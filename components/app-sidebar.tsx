dfa227cde1ddee4f3a99126b7b70c3da16fbde30
zack <miguel.quispe@fractal.com.pe>
2026-06-25 12:10:38 -0500

    refactor: remove unused components and optimize imports
    - Deleted `chart-area-interactive.tsx` and `section-cards.tsx` as they were no longer needed.
    - Removed `data-table.tsx` to streamline the data handling process.
    - Updated `nav-main.tsx` to simplify the sidebar structure and improve readability.

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -16,6 +16,8 @@ import {
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
 import { LayoutDashboardIcon, ListIcon, ChartBarIcon, FolderIcon, UsersIcon, CameraIcon, FileTextIcon, Settings2Icon, CircleHelpIcon, SearchIcon, DatabaseIcon, FileChartColumnIcon, FileIcon, CommandIcon } from "lucide-react"
+import { Icon, Trophy } from 'lucide-react';
+import { soccerBall } from '@lucide/lab';
 
 const data = {
   user: {
@@ -30,6 +32,13 @@ const data = {
       icon: (
         <LayoutDashboardIcon
         />
+      ),
+    },
+    {
+      title: "Campeonato",
+      url: "/dashboard/campeonato",
+      icon: (
+        <Trophy/>
       ),
     },
     {
@@ -186,9 +195,9 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
               asChild
               className="data-[slot=sidebar-menu-button]:p-1.5!"
             >
-              <a href="#">
-                <CommandIcon className="size-5!" />
-                <span className="text-base font-semibold">Acme Inc.</span>
+              <a href="#">                
+                <Icon iconNode={soccerBall} className="size-5!"/>
+                <span className="text-base font-semibold">Futbol Manager.</span>
               </a>
             </SidebarMenuButton>
           </SidebarMenuItem>
