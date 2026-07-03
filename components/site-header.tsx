83b36ce52276201c6c29532ef2ff8bd47702e024
zack <miguel.quispe@fractal.com.pe>
2026-07-03 14:34:19 -0500

    feat: simplify UserButton component in SiteHeader

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -18,9 +18,7 @@ export function SiteHeader() {
         <div className="flex items-center gap-2 ml-auto">
           <ThemeToggle />
           <Show when="signed-in">            
-              <UserButton
-                afterSignOutUrl="/"                
-              />            
+              <UserButton/>            
           </Show>
         </div>
         
